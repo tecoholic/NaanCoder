6e94a58a5e8af5407ecbe687551b9abc945e198b
Arunmozhi <arun@arunmozhi.in>
2019-11-14 10:33:05 +0530

    configured application to use language based urls

--- NaanCoder/settings.py
+++ NaanCoder/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -106,11 +107,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, "locale"),
+]
 
 USE_L10N = True
 
